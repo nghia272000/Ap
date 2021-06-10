32b33a4213f6ddb19c705338c338a40665cf4a5a
nghia272000 <71159935+nghia272000@users.noreply.github.com>
2021-06-10 09:44:35 +0700

    up code

---
+++ T2009M/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace T2009M
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("hello world!");
+        }
+    }
+}
